cc8536c3d43b5fdb5db117aa59550d1df75d6d6f
Denis Karlinsky <64307212+fondunn@users.noreply.github.com>
2022-03-19 20:09:10 +0200

    init repo

---
+++ README.md
@@ -0,0 +1,5 @@
+this is my react app without CRA
+
+git clone https://github.com/fondunn/dk30.git
+npm i
+npm start
---
+++ src/app.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from '@components/Header'
+
+function App() {
+    return (
+        <div>
+            <Header />
+            Create React App Without CRA
+            Testing reactive render
+        </div>
+    )
+}
+
+export default App
